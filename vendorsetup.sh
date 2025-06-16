2f3b3c946e269b3035c134fbc0327214c3a45594
J <40410860+Seriousattempts@users.noreply.github.com>
2025-06-16 02:58:25 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_ums512_1h10-user
-add_lunch_combo omni_ums512_1h10-userdebug
-add_lunch_combo omni_ums512_1h10-eng
+add_lunch_combo twrp_ums512_1h10-user
+add_lunch_combo twrp_ums512_1h10-userdebug
+add_lunch_combo twrp_ums512_1h10-eng
