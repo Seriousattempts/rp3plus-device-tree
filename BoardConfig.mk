4d2de1146cb1caf51e13ab4b5b0b9320a3ce7988
J <40410860+Seriousattempts@users.noreply.github.com>
2025-06-16 13:35:14 -0400

    Update BoardConfig.mk
    Display & Encryption support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,9 +105,18 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
-TW_THEME := portrait_hdpi
+TW_THEME := landscape_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
+
+# Display/Brightness Configuration
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 150
+
+# Encryption Support
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_USE_FSCRYPT_POLICY := 1
